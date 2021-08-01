9f3f9a154bd7fcb3f491a0be1e337edb81d6fd32
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-08-01 08:57:04 +0100

    added home binding

--- lib/cores/utils/locator.dart
+++ lib/cores/utils/locator.dart
@@ -1,6 +1,8 @@
 import 'package:elite/features/auth/binding/auth_binding.dart';
+import 'package:elite/features/home/binding/home_bindings.dart';
 
 /// init all binding
 void setUpLocator() {
   AuthenticationBinding().dependencies();
+  HomeBinding().dependencies();
 }
