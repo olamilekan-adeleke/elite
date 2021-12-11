29341947b0095c9d0bf80438fd2d97e279f2b446
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-12-11 17:31:02 +0100

    update

--- lib/features/home/views/widgets/home_body_widet.dart
+++ lib/features/home/views/widgets/home_body_widet.dart
@@ -1,3 +1,5 @@
+import 'package:elite/cores/components/custom_button.dart';
+import 'package:elite/cores/components/custom_text_widget.dart';
 import 'package:elite/cores/components/image_widget.dart';
 import 'package:elite/cores/utils/emums.dart';
 import 'package:elite/features/e_queue/views/screens/termial_list_screen.dart';
@@ -10,9 +12,6 @@ import 'home_header_widget.dart';
 
 class HomeBodyWidget extends StatelessWidget {
   const HomeBodyWidget({Key? key}) : super(key: key);
-
-  static final ProfileController profileController =
-      Get.find<ProfileController>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,57 +26,89 @@ class HomeBodyWidget extends StatelessWidget {
             height: sizerHeight(45),
             child: const TerminalsListScreen(),
           ),
-          Expanded(
-            child: Material(
-              elevation: 10.0,
-              color: Colors.transparent,
-              shadowColor: Colors.transparent,
-              child: Container(
-                padding: EdgeInsets.all(sizerSp(10)),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(sizerSp(25)),
-                    topRight: Radius.circular(sizerSp(25)),
-                  ),
-                  color: Colors.white,
-                ),
-                child: Obx(() {
-                  if (profileController.controllerState.value ==
-                      ControllerState.busy) {
-                    return const CircularProgressIndicator();
-                  }
-                  return Column(
-                    children: <Widget>[
-                      const SizedBox(width: double.infinity),
-                      if ((profileController
-                                  .userDetailsModel?.value.profilePicUrl ??
-                              '')
-                          .isNotEmpty)
-                        SizedBox(
-                          height: sizerSp(60),
-                          width: sizerSp(60),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(sizerSp(100)),
-                            child: CustomImageWidget(
-                              imageUrl: profileController
-                                      .userDetailsModel?.value.profilePicUrl ??
-                                  '',
-                              imageTypes: ImageTypes.network,
-                            ),
-                          ),
-                        )
-                      else
-                        CircleAvatar(
-                          radius: sizerSp(30),
-                          child: const Icon(Icons.person),
-                        ),
-                    ],
-                  );
-                }),
-              ),
-            ),
-          ),
+          const HomeProfileWidget(),
         ],
+      ),
+    );
+  }
+}
+
+class HomeProfileWidget extends StatelessWidget {
+  const HomeProfileWidget({Key? key}) : super(key: key);
+
+  static final ProfileController profileController =
+      Get.find<ProfileController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        elevation: 10.0,
+        color: Colors.transparent,
+        shadowColor: Colors.transparent,
+        child: Container(
+          padding: EdgeInsets.all(sizerSp(10)),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(sizerSp(25)),
+              topRight: Radius.circular(sizerSp(25)),
+            ),
+            color: Colors.white,
+          ),
+          child: Obx(
+            () {
+              if (profileController.controllerState.value ==
+                  ControllerState.busy) {
+                return const CircularProgressIndicator();
+              }
+              return Column(
+                children: <Widget>[
+                  const SizedBox(width: double.infinity),
+                  if ((profileController
+                              .userDetailsModel?.value.profilePicUrl ??
+                          '')
+                      .isNotEmpty)
+                    SizedBox(
+                      height: sizerSp(60),
+                      width: sizerSp(60),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(sizerSp(100)),
+                        child: CustomImageWidget(
+                          imageUrl: profileController
+                                  .userDetailsModel?.value.profilePicUrl ??
+                              '',
+                          imageTypes: ImageTypes.network,
+                        ),
+                      ),
+                    )
+                  else
+                    CircleAvatar(
+                      radius: sizerSp(30),
+                      child: const Icon(Icons.person),
+                    ),
+                  SizedBox(height: sizerSp(15)),
+                  CustomTextWidget(
+                    'Good afternoon, Joy',
+                    fontSize: sizerSp(17),
+                    fontWeight: FontWeight.w300,
+                  ),
+                  SizedBox(height: sizerSp(10)),
+                  CustomTextWidget(
+                    'Going Off-campus?',
+                    fontSize: sizerSp(25),
+                    fontWeight: FontWeight.bold,
+                  ),
+                  const Spacer(),
+                  CustomButton(
+                    text: 'Get a ride',
+                    onTap: () {},
+                  ),
+                  SizedBox(height: sizerSp(15)),
+                ],
+              );
+            },
+          ),
+        ),
       ),
     );
   }
