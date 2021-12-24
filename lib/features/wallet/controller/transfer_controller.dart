74f0c22aa30ab74d9de7aad7cd9c0dd8d6ab1e31
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-12-24 02:17:27 +0100

    update

--- lib/features/wallet/controller/transfer_controller.dart
+++ lib/features/wallet/controller/transfer_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:elite/cores/utils/emums.dart';
 import 'package:elite/cores/utils/snack_bar_service.dart';
+import 'package:elite/features/auth/model/user_details_model.dart';
 import 'package:elite/features/wallet/services/transfer_services.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,6 +11,7 @@ class TransferController extends GetxController {
   static final TransferServices transferServices = TransferServices();
   final Rx<String> type = 'Cash'.obs;
   final Rx<String> receivedAs = 'Cash'.obs;
+  Rx<UserDetailsModel>? receiverDetails;
   final Rx<ControllerState> state = ControllerState.init.obs;
   final TextEditingController amountController =
       TextEditingController(text: '');
@@ -20,7 +24,7 @@ class TransferController extends GetxController {
     // turn type to lower case
   }
 
-  void proceed() {
+  Future<void> proceed() async {
     if (amountController.text.isEmpty) {
       return showWarningSnackBar('Amount cannot be empty!');
     }
@@ -35,7 +39,25 @@ class TransferController extends GetxController {
 
     // send request to backend and backend return user details
 
-    Get.toNamed('/confirm-transfer');
+    try {
+      state.value = ControllerState.busy;
+
+      final UserDetailsModel? _receiverDetails =
+          await transferServices.getUserDetails(usernameController.text.trim());
+
+      if (_receiverDetails != null) {
+        receiverDetails = Rx<UserDetailsModel>(_receiverDetails);
+        Get.toNamed('/confirm-transfer');
+      }
+
+      state.value = ControllerState.success;
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+      state.value = ControllerState.error;
+
+      showErrorSnackBar(e.toString());
+    }
   }
 
   void showPopUp() {}
--- lib/features/wallet/services/transfer_services.dart
+++ lib/features/wallet/services/transfer_services.dart
@@ -1,3 +1,36 @@
-class TransferServices{
+import 'dart:convert';
+import 'dart:developer';
 
+import 'package:elite/cores/constants/api.dart';
+import 'package:elite/features/auth/model/user_details_model.dart';
+import 'package:http/http.dart' as http;
+
+class TransferServices {
+  Future<UserDetailsModel?> getUserDetails(String username) async {
+    try {
+      final http.Response response = await http.post(
+        Uri.parse('$firebaseBaseUrl/initSendFund'),
+        body: <String, dynamic>{'receiver_username': username},
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      );
+
+      final Map<String, dynamic> responseData =
+          json.decode(response.body) as Map<String, dynamic>;
+
+      if (responseData['status'] == 'success') {
+        return UserDetailsModel.fromMap(
+            responseData['data'] as Map<String, dynamic>);
+      } else {
+        throw responseData['msg'].toString();
+      }
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+
+      throw e.toString();
+    }
+  }
 }
